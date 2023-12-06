d1efa2a14faf25a43e51a20d114277827b0e6f4f
patcunko <71910223+patcunko@users.noreply.github.com>
2023-12-06 17:42:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import flet as ft
+from flet import *
+from fletFunctions import start_page
 
+txt_name = ft.TextField(
+    label="Summoner#xxxx",
+    bgcolor=colors.WHITE,
+    width=267)
+
+
+regiondd = ft.Dropdown(
+    width=100,
+    options=[
+        ft.dropdown.Option("NA1"),
+        ft.dropdown.Option("EUW1"),
+        ft.dropdown.Option("EUN1"),
+        ft.dropdown.Option("KR"),
+        ft.dropdown.Option("BR1"),
+    ],
+)
+dd = ft.Container(
+    content=regiondd,
+    bgcolor=colors.WHITE
+)
+
+
+def main(page: Page):
+    start_page(page)
+
+
+ft.app(target=main)
